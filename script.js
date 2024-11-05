ff343fd16962fa6d407e713331096b0af2fed53d
ilyass anida <anidailyass@gmail.com>
2024-11-05 22:00:14 +0100

    Load data using Ajax.

--- script.js
+++ script.js
@@ -6,33 +6,8 @@ const swipedImagesBtn = document.getElementById("swiped-images-btn");
 const likedImagesBtn = document.getElementById("liked-images-btn");
 const waitingImagesBtn = document.getElementById("waiting-images-btn");
 
-waitingImagesBtn.addEventListener("click", function() {
-    cards.classList.remove("d-none")
-    likedCards.classList.add("d-none");
-    swipedCards.classList.add("d-none");
-});
-swipedImagesBtn.addEventListener("click", function() {
-    cards.classList.add("d-none")
-    likedCards.classList.add("d-none");
-    swipedCards.classList.remove("d-none");
-});
-likedImagesBtn.addEventListener("click", function() {
-    cards.classList.add("d-none")
-    likedCards.classList.remove("d-none");
-    swipedCards.classList.add("d-none");
-});
-
-const swipeBtns = document.querySelectorAll(".swipe-btn");
-const likeBtns = document.querySelectorAll(".like-btn");
-
+loadCards();
 updateCounts();
-
-swipeBtns.forEach(btn => {
-    btn.addEventListener("click", () => swipe(btn.parentElement.parentElement.parentElement))
-})
-likeBtns.forEach(btn => {
-    btn.addEventListener("click", () => like(btn.parentElement.parentElement.parentElement))
-})
 
 function swipe(card) {
     let cloneCard = card.cloneNode(true);
@@ -73,4 +48,61 @@ function updateCounts() {
     swipedImagesBtn.querySelector(".count").textContent = swipedCards.children.length;
     likedImagesBtn.querySelector(".count").textContent = likedCards.children.length;
     waitingImagesBtn.querySelector(".count").textContent = Array.from(cards.children).filter(card => !card.hasAttribute("data-reaction")).length;
+}
+
+function applyEvents() {
+    const swipeBtns = document.querySelectorAll(".swipe-btn");
+    const likeBtns = document.querySelectorAll(".like-btn");
+
+    waitingImagesBtn.addEventListener("click", function() {
+        cards.classList.remove("d-none")
+        likedCards.classList.add("d-none");
+        swipedCards.classList.add("d-none");
+    });
+    swipedImagesBtn.addEventListener("click", function() {
+        cards.classList.add("d-none")
+        likedCards.classList.add("d-none");
+        swipedCards.classList.remove("d-none");
+    });
+    likedImagesBtn.addEventListener("click", function() {
+        cards.classList.add("d-none")
+        likedCards.classList.remove("d-none");
+        swipedCards.classList.add("d-none");
+    });
+
+    swipeBtns.forEach(btn => {
+        btn.addEventListener("click", () => swipe(btn.parentElement.parentElement.parentElement))
+    })
+    likeBtns.forEach(btn => {
+        btn.addEventListener("click", () => like(btn.parentElement.parentElement.parentElement))
+    })
+
+    updateCounts();
+}
+
+function loadCards() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "./data.json");
+    xhr.send();
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            let data = JSON.parse(this.responseText);
+
+            for (let i = 0; i < data.length; i++) {
+                cards.innerHTML += `
+                    <div class="card" style="--i: ${i <= 4 ? i : 4}">
+                            <img src="${data[i].image}" alt="${data[i].name}">
+                            <div class="content">
+                                <h2 class="text-center py-2 display-1">${data[i].name}</h2>
+                                <div class="controll d-flex justify-content-between">
+                                    <span class="swipe-btn bg-white rounded-circle display-2"><i style="rotate: 180deg;" class="fa-solid fa-thumbs-up text-secondary"></i></span>
+                                    <span class="like-btn bg-white rounded-circle display-2"><i class="fa-solid fa-heart text-danger"></i></span>
+                                </div>
+                            </div>
+                    </div>`
+            }
+            applyEvents();
+        }
+    }
 }
